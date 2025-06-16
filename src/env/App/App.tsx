d11cfae76b91b01ef950e828a47fb4059bb04a71
Viraj Perera <viraj.perera.development@gmail.com>
2025-06-16 11:40:34 +0200

    fix button version

--- src/env/App/App.tsx
+++ src/env/App/App.tsx
@@ -5,21 +5,27 @@ import './index.css';
 const App: FC = () => {
     return (
         <div style={{ width: '100vw', height: '200vh' }}>
-        <div
-            style={{
-                width: '100%',
-                height: '200vh',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: "url('https://picsum.photos/1200/800')",
-                position: 'relative',
-            }}
-        >
-        </div>
+            <div
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: "url('https://picsum.photos/1200/800')",
+                    position: 'relative',
+                }}
+            >
+                <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                <GlassCard>
+      <div className="p-6">
+        <h2>Welcome to Liquid Glass</h2>
+        <p>Experience the future of UI design with smooth, organic animations.</p>
+      </div>
+    </GlassCard>
+                </div>
+            </div>
 
-        <GlassCard style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            <h1 style={{ color: 'white' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        </GlassCard>
+
         </div>
     );
 };
